45881865869f02437aa708674341e9a600dff440
Petr Timofeev <p.timofeev@rficb.ru>
2017-03-11 09:48:55 +0300

    python3 support

--- celery_pubsub/__init__.py
+++ celery_pubsub/__init__.py
@@ -1,3 +1,3 @@
-from pubsub import publish_now, publish, subscribe, unsubscribe
+from .pubsub import publish_now, publish, subscribe, unsubscribe
 
 __all__ = ['publish_now', 'publish', 'subscribe', 'unsubscribe', ]
