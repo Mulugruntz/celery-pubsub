f50aabff11f9c2008b25ed7ad1a390e4c185c77a
Samuel Giffard <samuel.giffard@unleashed.be>
2017-11-02 13:19:53 +0100

    Version bump 0.1.7 -> 0.1.8

--- setup.py
+++ setup.py
@@ -44,14 +44,14 @@ class nosetest(setuptools.command.test.test):
 setuptools.setup(
     name='celery-pubsub',
     packages=['celery_pubsub'],
-    version='0.1.7',
+    version='0.1.8',
     description='A Publish and Subscribe library for Celery',
     long_description=long_description(),
     author='Samuel GIFFARD',
     author_email='mulugruntz@gmail.com',
     license='MIT',
     url='https://github.com/Mulugruntz/celery-pubsub',
-    download_url='https://github.com/Mulugruntz/celery-pubsub/tarball/0.1.7',
+    download_url='https://github.com/Mulugruntz/celery-pubsub/tarball/0.1.8',
     keywords=['celery', 'publish', 'subscribe', 'pubsub'],
     classifiers=[
         'Development Status :: 3 - Alpha',
