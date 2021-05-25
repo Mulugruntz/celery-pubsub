a8753e4eb90a04574fd178d2fcad3f1a65ec5cb5
Samuel GIFFARD <mulugruntz@gmail.com>
2021-05-26 03:33:46 +0900

    Auto-reformat with Black.

--- setup.py
+++ setup.py
@@ -7,75 +7,77 @@ import sys
 
 def long_description():
     try:
-        return codecs.open('README.rst', 'r', 'utf-8').read()
+        return codecs.open("README.rst", "r", "utf-8").read()
     except IOError:
-        return 'Long description error: Missing README.rst file'
+        return "Long description error: Missing README.rst file"
 
 
 def _strip_comments(l):
-    return l.split('#', 1)[0].strip()
+    return l.split("#", 1)[0].strip()
 
 
 def parse_req_file(filename):
     full_path = os.path.join(os.getcwd(), filename)
     return [
         _strip_comments(req)
-        for req in codecs.open(full_path, 'r', 'utf-8').readlines() if req
+        for req in codecs.open(full_path, "r", "utf-8").readlines()
+        if req
     ]
 
 
 def install_requires():
-    return parse_req_file('requirements.txt')
+    return parse_req_file("requirements.txt")
 
 
 def tests_require():
-    return parse_req_file('requirements_test.txt')
+    return parse_req_file("requirements_test.txt")
 
 
 class nosetest(setuptools.command.test.test):
     def initialize_options(self):
         setuptools.command.test.test.initialize_options(self)
         self.argv = [
-            '--cover-branches',
-            '--with-coverage',
-            '--cover-xml',
-            '--cover-inclusive',
-            '--cover-erase',
-            '--cover-package=celery_pubsub',
-            'tests/pubsub.py',
+            "--cover-branches",
+            "--with-coverage",
+            "--cover-xml",
+            "--cover-inclusive",
+            "--cover-erase",
+            "--cover-package=celery_pubsub",
+            "tests/pubsub.py",
         ]
 
     def run_tests(self):
         import nose
+
         sys.exit(nose.main(argv=self.argv))
 
 
 setuptools.setup(
-    name='celery-pubsub',
-    packages=['celery_pubsub'],
-    version='0.2.1',
-    description='A Publish and Subscribe library for Celery',
+    name="celery-pubsub",
+    packages=["celery_pubsub"],
+    version="0.2.1",
+    description="A Publish and Subscribe library for Celery",
     long_description=long_description(),
-    author='Samuel GIFFARD',
-    author_email='mulugruntz@gmail.com',
-    license='MIT',
-    url='https://github.com/Mulugruntz/celery-pubsub',
-    download_url='https://github.com/Mulugruntz/celery-pubsub/tarball/0.2.1',
-    keywords=['celery', 'publish', 'subscribe', 'pubsub'],
+    author="Samuel GIFFARD",
+    author_email="mulugruntz@gmail.com",
+    license="MIT",
+    url="https://github.com/Mulugruntz/celery-pubsub",
+    download_url="https://github.com/Mulugruntz/celery-pubsub/tarball/0.2.1",
+    keywords=["celery", "publish", "subscribe", "pubsub"],
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Topic :: Software Development :: Libraries',
-        'Topic :: System :: Distributed Computing',
-        'Topic :: Utilities',
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: System :: Distributed Computing",
+        "Topic :: Utilities",
     ],
-    cmdclass={'test': nosetest},
+    cmdclass={"test": nosetest},
     include_package_data=True,
     install_requires=install_requires(),
     tests_require=tests_require(),
--- tests/pubsub.py
+++ tests/pubsub.py
@@ -6,7 +6,7 @@ celery.current_app.conf.update(
     CELERY_ALWAYS_EAGER=True,
 )
 
-if celery.__version__ < '4.0.0':  # pragma: no cover
+if celery.__version__ < "4.0.0":  # pragma: no cover
     task = celery.task
 else:  # pragma: no cover
     app = celery.Celery()
@@ -55,80 +55,91 @@ def job_g(*args, **kwargs):
     return "g"
 
 
-celery_pubsub.subscribe('index.high', job_a)
-celery_pubsub.subscribe('index.low', job_b)
-celery_pubsub.subscribe('index', job_c)
-celery_pubsub.subscribe('index.#', job_d)
-celery_pubsub.subscribe('#', job_e)
-celery_pubsub.subscribe('index.*.test', job_f)
-celery_pubsub.subscribe('index.#.test', job_g)
+celery_pubsub.subscribe("index.high", job_a)
+celery_pubsub.subscribe("index.low", job_b)
+celery_pubsub.subscribe("index", job_c)
+celery_pubsub.subscribe("index.#", job_d)
+celery_pubsub.subscribe("#", job_e)
+celery_pubsub.subscribe("index.*.test", job_f)
+celery_pubsub.subscribe("index.#.test", job_g)
 
 
 class PubsubTest(unittest.TestCase):
     def test_subscription(self):
         from celery_pubsub import publish, subscribe
-        res = publish('dummy', 4, 8, a15=16, a23=42).get()
-        self.assertListEqual(sorted(res), sorted(['e']))
 
-        subscribe('dummy', job_c)
+        res = publish("dummy", 4, 8, a15=16, a23=42).get()
+        self.assertListEqual(sorted(res), sorted(["e"]))
 
-        res = publish('dummy', 4, 8, a15=16, a23=42).get()
-        self.assertListEqual(sorted(res), sorted(['e', 'c']))
+        subscribe("dummy", job_c)
 
-        celery_pubsub.unsubscribe('dummy', job_c)
+        res = publish("dummy", 4, 8, a15=16, a23=42).get()
+        self.assertListEqual(sorted(res), sorted(["e", "c"]))
 
-        res = publish('dummy', 4, 8, a15=16, a23=42).get()
-        self.assertListEqual(sorted(res), sorted(['e']))
+        celery_pubsub.unsubscribe("dummy", job_c)
+
+        res = publish("dummy", 4, 8, a15=16, a23=42).get()
+        self.assertListEqual(sorted(res), sorted(["e"]))
 
     def test_1(self):
-        res = celery_pubsub.publish('index.low.test', 4, 8, a15=16, a23=42).get()
-        self.assertListEqual(sorted(res), sorted(['d', 'e', 'f', 'g']))
+        res = celery_pubsub.publish("index.low.test", 4, 8, a15=16, a23=42).get()
+        self.assertListEqual(sorted(res), sorted(["d", "e", "f", "g"]))
 
     def test_2(self):
-        res = celery_pubsub.publish('something.else', 4, 8, a15=16, a23=42).get()
-        self.assertListEqual(sorted(res), sorted(['e']))
+        res = celery_pubsub.publish("something.else", 4, 8, a15=16, a23=42).get()
+        self.assertListEqual(sorted(res), sorted(["e"]))
 
     def test_3(self):
-        res = celery_pubsub.publish('index.high', 4, 8, a15=16, a23=42).get()
-        self.assertListEqual(sorted(res), sorted(['a', 'd', 'e']))
+        res = celery_pubsub.publish("index.high", 4, 8, a15=16, a23=42).get()
+        self.assertListEqual(sorted(res), sorted(["a", "d", "e"]))
 
     def test_4(self):
-        res = celery_pubsub.publish_now('index', 4, 8, a15=16, a23=42).get()
-        self.assertListEqual(sorted(res), sorted(['c', 'e']))
+        res = celery_pubsub.publish_now("index", 4, 8, a15=16, a23=42).get()
+        self.assertListEqual(sorted(res), sorted(["c", "e"]))
 
     def test_5(self):
         from celery_pubsub import publish as pub
-        res = pub('index.low', 4, 8, a15=16, a23=42).get()
-        self.assertListEqual(sorted(res), sorted(['b', 'd', 'e']))
+
+        res = pub("index.low", 4, 8, a15=16, a23=42).get()
+        self.assertListEqual(sorted(res), sorted(["b", "d", "e"]))
 
     def test_6(self):
         import celery_pubsub as pubsub
-        res = pubsub.publish('index.high.some.test', 4, 8, a15=16, a23=42).get()
-        self.assertListEqual(sorted(res), sorted(['d', 'e', 'g']))
+
+        res = pubsub.publish("index.high.some.test", 4, 8, a15=16, a23=42).get()
+        self.assertListEqual(sorted(res), sorted(["d", "e", "g"]))
 
     def test_7(self):
         from celery_pubsub import publish
-        res = publish('index.high.test', 4, 8, a15=16, a23=42).get()
-        self.assertListEqual(sorted(res), sorted(['d', 'e', 'f', 'g']))
+
+        res = publish("index.high.test", 4, 8, a15=16, a23=42).get()
+        self.assertListEqual(sorted(res), sorted(["d", "e", "f", "g"]))
 
     def test_subscription_redundant(self):
-        jobs_init = celery_pubsub.pubsub._pubsub_manager.get_jobs('redundant.test').tasks
-        celery_pubsub.subscribe('redundant.test', job_a)
-        jobs_before = celery_pubsub.pubsub._pubsub_manager.get_jobs('redundant.test').tasks
-        celery_pubsub.subscribe('redundant.test', job_a)
-        jobs_after = celery_pubsub.pubsub._pubsub_manager.get_jobs('redundant.test').tasks
-        celery_pubsub.unsubscribe('redundant.test', job_a)
-        jobs_end = celery_pubsub.pubsub._pubsub_manager.get_jobs('redundant.test').tasks
+        jobs_init = celery_pubsub.pubsub._pubsub_manager.get_jobs(
+            "redundant.test"
+        ).tasks
+        celery_pubsub.subscribe("redundant.test", job_a)
+        jobs_before = celery_pubsub.pubsub._pubsub_manager.get_jobs(
+            "redundant.test"
+        ).tasks
+        celery_pubsub.subscribe("redundant.test", job_a)
+        jobs_after = celery_pubsub.pubsub._pubsub_manager.get_jobs(
+            "redundant.test"
+        ).tasks
+        celery_pubsub.unsubscribe("redundant.test", job_a)
+        jobs_end = celery_pubsub.pubsub._pubsub_manager.get_jobs("redundant.test").tasks
 
         self.assertListEqual(jobs_before, jobs_after)
         self.assertListEqual(jobs_init, jobs_end)
 
     def test_unsubscribe_nonexistant(self):
-        jobs_before = celery_pubsub.pubsub._pubsub_manager.get_jobs('not.exists').tasks
-        celery_pubsub.unsubscribe('not.exists', job_a)
-        jobs_after = celery_pubsub.pubsub._pubsub_manager.get_jobs('not.exists').tasks
+        jobs_before = celery_pubsub.pubsub._pubsub_manager.get_jobs("not.exists").tasks
+        celery_pubsub.unsubscribe("not.exists", job_a)
+        jobs_after = celery_pubsub.pubsub._pubsub_manager.get_jobs("not.exists").tasks
 
         self.assertListEqual(jobs_before, jobs_after)
 
-if __name__ == '__main__':  # pragma: no cover
+
+if __name__ == "__main__":  # pragma: no cover
     unittest.main()
