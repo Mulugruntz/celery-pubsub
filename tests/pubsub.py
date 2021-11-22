12626cdb403f45cd05affd71c3bba74c18b8a83a
Raphael Lullis <raphael@mushroomlabs.com>
2021-11-22 13:34:33 +0100

    Remove dependency pinning. Closes #127
    In order for this module to be used as a library, it will be better if
    we remove any dependency pinning.
    
    This PR also updates some of the project configuration. It adds
    support for [black](https://black.readthedocs.io/) and flake8 for
    automated formatting and linting.

--- tests/pubsub.py
+++ tests/pubsub.py
@@ -1,128 +1,140 @@
 import unittest
-import celery
+
 import celery_pubsub
-
-celery.current_app.conf.update(
-    CELERY_ALWAYS_EAGER=True,
-)
+from celery import Celery
 
 
-@celery.task
+class TestConfig:
+    name = "PubSubTester"
+    broker_url = "memory"
+    task_always_eager = True
+
+
+app = Celery()
+app.config_from_object(TestConfig)
+
+
+@app.task
 def job_a(*args, **kwargs):
     print("job_a: {} {}".format(args, kwargs))
     return "a"
 
 
-@celery.task
+@app.task
 def job_b(*args, **kwargs):
     print("job_b: {} {}".format(args, kwargs))
     return "b"
 
 
-@celery.task
+@app.task
 def job_c(*args, **kwargs):
     print("job_c: {} {}".format(args, kwargs))
     return "c"
 
 
-@celery.task
+@app.task
 def job_d(*args, **kwargs):
     print("job_d: {} {}".format(args, kwargs))
     return "d"
 
 
-@celery.task
+@app.task
 def job_e(*args, **kwargs):
     print("job_e: {} {}".format(args, kwargs))
     return "e"
 
 
-@celery.task
+@app.task
 def job_f(*args, **kwargs):
     print("job_f: {} {}".format(args, kwargs))
     return "f"
 
 
-@celery.task
+@app.task
 def job_g(*args, **kwargs):
     print("job_g: {} {}".format(args, kwargs))
     return "g"
 
 
-celery_pubsub.subscribe('index.high', job_a)
-celery_pubsub.subscribe('index.low', job_b)
-celery_pubsub.subscribe('index', job_c)
-celery_pubsub.subscribe('index.#', job_d)
-celery_pubsub.subscribe('#', job_e)
-celery_pubsub.subscribe('index.*.test', job_f)
-celery_pubsub.subscribe('index.#.test', job_g)
+celery_pubsub.subscribe("index.high", job_a)
+celery_pubsub.subscribe("index.low", job_b)
+celery_pubsub.subscribe("index", job_c)
+celery_pubsub.subscribe("index.#", job_d)
+celery_pubsub.subscribe("#", job_e)
+celery_pubsub.subscribe("index.*.test", job_f)
+celery_pubsub.subscribe("index.#.test", job_g)
 
 
 class PubsubTest(unittest.TestCase):
     def test_subscription(self):
         from celery_pubsub import publish, subscribe
-        res = publish('dummy', 4, 8, a15=16, a23=42).get()
-        self.assertListEqual(sorted(res), sorted(['e']))
 
-        subscribe('dummy', job_c)
+        res = publish("dummy", 4, 8, a15=16, a23=42).get()
+        self.assertListEqual(sorted(res), sorted(["e"]))
 
-        res = publish('dummy', 4, 8, a15=16, a23=42).get()
-        self.assertListEqual(sorted(res), sorted(['e', 'c']))
+        subscribe("dummy", job_c)
 
-        celery_pubsub.unsubscribe('dummy', job_c)
+        res = publish("dummy", 4, 8, a15=16, a23=42).get()
+        self.assertListEqual(sorted(res), sorted(["e", "c"]))
 
-        res = publish('dummy', 4, 8, a15=16, a23=42).get()
-        self.assertListEqual(sorted(res), sorted(['e']))
+        celery_pubsub.unsubscribe("dummy", job_c)
+
+        res = publish("dummy", 4, 8, a15=16, a23=42).get()
+        self.assertListEqual(sorted(res), sorted(["e"]))
 
     def test_1(self):
-        res = celery_pubsub.publish('index.low.test', 4, 8, a15=16, a23=42).get()
-        self.assertListEqual(sorted(res), sorted(['d', 'e', 'f', 'g']))
+        res = celery_pubsub.publish("index.low.test", 4, 8, a15=16, a23=42).get()
+        self.assertListEqual(sorted(res), sorted(["d", "e", "f", "g"]))
 
     def test_2(self):
-        res = celery_pubsub.publish('something.else', 4, 8, a15=16, a23=42).get()
-        self.assertListEqual(sorted(res), sorted(['e']))
+        res = celery_pubsub.publish("something.else", 4, 8, a15=16, a23=42).get()
+        self.assertListEqual(sorted(res), sorted(["e"]))
 
     def test_3(self):
-        res = celery_pubsub.publish('index.high', 4, 8, a15=16, a23=42).get()
-        self.assertListEqual(sorted(res), sorted(['a', 'd', 'e']))
+        res = celery_pubsub.publish("index.high", 4, 8, a15=16, a23=42).get()
+        self.assertListEqual(sorted(res), sorted(["a", "d", "e"]))
 
     def test_4(self):
-        res = celery_pubsub.publish_now('index', 4, 8, a15=16, a23=42).get()
-        self.assertListEqual(sorted(res), sorted(['c', 'e']))
+        res = celery_pubsub.publish_now("index", 4, 8, a15=16, a23=42).get()
+        self.assertListEqual(sorted(res), sorted(["c", "e"]))
 
     def test_5(self):
         from celery_pubsub import publish as pub
-        res = pub('index.low', 4, 8, a15=16, a23=42).get()
-        self.assertListEqual(sorted(res), sorted(['b', 'd', 'e']))
+
+        res = pub("index.low", 4, 8, a15=16, a23=42).get()
+        self.assertListEqual(sorted(res), sorted(["b", "d", "e"]))
 
     def test_6(self):
         import celery_pubsub as pubsub
-        res = pubsub.publish('index.high.some.test', 4, 8, a15=16, a23=42).get()
-        self.assertListEqual(sorted(res), sorted(['d', 'e', 'g']))
+
+        res = pubsub.publish("index.high.some.test", 4, 8, a15=16, a23=42).get()
+        self.assertListEqual(sorted(res), sorted(["d", "e", "g"]))
 
     def test_7(self):
         from celery_pubsub import publish
-        res = publish('index.high.test', 4, 8, a15=16, a23=42).get()
-        self.assertListEqual(sorted(res), sorted(['d', 'e', 'f', 'g']))
+
+        res = publish("index.high.test", 4, 8, a15=16, a23=42).get()
+        self.assertListEqual(sorted(res), sorted(["d", "e", "f", "g"]))
 
     def test_subscription_redundant(self):
-        jobs_init = celery_pubsub.pubsub._pubsub_manager.get_jobs('redundant.test').tasks
-        celery_pubsub.subscribe('redundant.test', job_a)
-        jobs_before = celery_pubsub.pubsub._pubsub_manager.get_jobs('redundant.test').tasks
-        celery_pubsub.subscribe('redundant.test', job_a)
-        jobs_after = celery_pubsub.pubsub._pubsub_manager.get_jobs('redundant.test').tasks
-        celery_pubsub.unsubscribe('redundant.test', job_a)
-        jobs_end = celery_pubsub.pubsub._pubsub_manager.get_jobs('redundant.test').tasks
+        jobs_init = celery_pubsub.pubsub._pubsub_manager.get_jobs("redundant.test").tasks
+        celery_pubsub.subscribe("redundant.test", job_a)
+        jobs_before = celery_pubsub.pubsub._pubsub_manager.get_jobs("redundant.test").tasks
+        celery_pubsub.subscribe("redundant.test", job_a)
+        jobs_after = celery_pubsub.pubsub._pubsub_manager.get_jobs("redundant.test").tasks
+        celery_pubsub.unsubscribe("redundant.test", job_a)
+        jobs_end = celery_pubsub.pubsub._pubsub_manager.get_jobs("redundant.test").tasks
 
         self.assertListEqual(jobs_before, jobs_after)
         self.assertListEqual(jobs_init, jobs_end)
 
     def test_unsubscribe_nonexistant(self):
-        jobs_before = celery_pubsub.pubsub._pubsub_manager.get_jobs('not.exists').tasks
-        celery_pubsub.unsubscribe('not.exists', job_a)
-        jobs_after = celery_pubsub.pubsub._pubsub_manager.get_jobs('not.exists').tasks
+        jobs_before = celery_pubsub.pubsub._pubsub_manager.get_jobs("not.exists").tasks
+        celery_pubsub.unsubscribe("not.exists", job_a)
+        jobs_after = celery_pubsub.pubsub._pubsub_manager.get_jobs("not.exists").tasks
 
         self.assertListEqual(jobs_before, jobs_after)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
