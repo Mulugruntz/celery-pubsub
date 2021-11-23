ef0169e856918ec75cc5d6c774106f467ab6eb43
Samuel GIFFARD <mulugruntz@gmail.com>
2021-11-23 18:46:59 +0900

    Tests now work properly for both celery3 and celery 4+.

--- tests/test_pubsub.py
+++ tests/test_pubsub.py
@@ -1,10 +1,8 @@
 from celery_pubsub import publish, unsubscribe, publish_now
 
 
-def test_subscription(subscriber, job_c):
+def test_subscription(subscriber, job_c, celery_worker):
     from celery_pubsub import publish, subscribe
-
-    subscriber()
 
     res = publish("dummy", 4, 8, a15=16, a23=42).get()
     assert sorted(res) == sorted(["e"])
@@ -20,60 +18,50 @@ def test_subscription(subscriber, job_c):
     assert sorted(res) == sorted(["e"])
 
 
-def test_1(subscriber):
-    subscriber()
+def test_1(subscriber, celery_worker):
     res = publish("index.low.test", 4, 8, a15=16, a23=42).get()
     assert sorted(res) == sorted(["d", "e", "f", "g"])
 
 
-def test_2(subscriber):
-    subscriber()
+def test_2(subscriber, celery_worker):
     res = publish("something.else", 4, 8, a15=16, a23=42).get()
     assert sorted(res) == sorted(["e"])
 
 
-def test_3(subscriber):
-    subscriber()
+def test_3(subscriber, celery_worker):
     res = publish("index.high", 4, 8, a15=16, a23=42).get()
     assert sorted(res) == sorted(["a", "d", "e"])
 
 
-def test_4(subscriber):
-    subscriber()
+def test_4(subscriber, celery_worker):
     res = publish_now("index", 4, 8, a15=16, a23=42).get()
     assert sorted(res) == sorted(["c", "e"])
 
 
-def test_5(subscriber):
-    subscriber()
+def test_5(subscriber, celery_worker):
     from celery_pubsub import publish as pub
 
     res = pub("index.low", 4, 8, a15=16, a23=42).get()
     assert sorted(res) == sorted(["b", "d", "e"])
 
 
-def test_6(subscriber):
+def test_6(subscriber, celery_worker):
     import celery_pubsub as pubsub
-
-    subscriber()
 
     res = pubsub.publish("index.high.some.test", 4, 8, a15=16, a23=42).get()
     assert sorted(res) == sorted(["d", "e", "g"])
 
 
-def test_7(subscriber):
+def test_7(subscriber, celery_worker):
     from celery_pubsub import publish
-
-    subscriber()
 
     res = publish("index.high.test", 4, 8, a15=16, a23=42).get()
     assert sorted(res) == sorted(["d", "e", "f", "g"])
 
 
-def test_subscription_redundant(subscriber, job_a):
+def test_subscription_redundant(subscriber, job_a, celery_worker):
     import celery_pubsub
 
-    subscriber()
     jobs_init = celery_pubsub.pubsub._pubsub_manager.get_jobs("redundant.test").tasks
     celery_pubsub.subscribe("redundant.test", job_a)
     jobs_before = celery_pubsub.pubsub._pubsub_manager.get_jobs("redundant.test").tasks
@@ -86,10 +74,9 @@ def test_subscription_redundant(subscriber, job_a):
     assert jobs_init == jobs_end
 
 
-def test_unsubscribe_nonexistant(subscriber, job_a):
+def test_unsubscribe_nonexistant(subscriber, job_a, celery_worker):
     import celery_pubsub
 
-    subscriber()
     jobs_before = celery_pubsub.pubsub._pubsub_manager.get_jobs("not.exists").tasks
     celery_pubsub.unsubscribe("not.exists", job_a)
     jobs_after = celery_pubsub.pubsub._pubsub_manager.get_jobs("not.exists").tasks
